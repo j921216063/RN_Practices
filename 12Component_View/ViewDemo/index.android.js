1e8772ceafeef79b502b8a75b53b566bd29c8554
Jack_You <saufay@mitake.com.tw>
2016-10-12 17:49:45 +0800

    12Component_View

---
+++ 12Component_View/ViewDemo/index.android.js
@@ -0,0 +1,85 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  PixelRatio,
+} from 'react-native';
+
+export default class ViewDemo extends Component {
+  render() {
+    return (
+      <View style={styles.flex}>
+        <View style={styles.container}>
+          <View style={[styles.item, styles.center]}>
+            <Text style={styles.font}>酒店</Text>
+          </View>
+          <View style={[styles.item, styles.lineLeftRight]}>
+            <View style={[styles.center, styles.flex, styles.lineCenter]}>
+              <Text style={styles.font}>海外酒店</Text>
+            </View>
+            <View style={[styles.center, styles.flex]}>
+              <Text style={styles.font}>特惠酒店</Text>
+            </View>
+          </View>
+        
+          <View style={[styles.item, styles.lineLeftRight]}>
+            <View style={[styles.center, styles.flex, styles.lineCenter]}>
+              <Text style={styles.font}>團購</Text>
+            </View>
+            <View style={[styles.center, styles.flex]}>
+              <Text style={styles.font}>客棧，公寓</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 200,
+    marginLeft: 5,
+    marginRight: 5,
+    height: 84,
+    flexDirection: 'row',
+    borderRadius: 5,
+    padding: 2,
+    backgroundColor: '#FF0067',
+  },
+  item: {
+    flex: 1,
+    height: 80,
+  },
+  flex: {
+    flex: 1,
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  font: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  lineLeftRight: {
+    borderLeftWidth: 1 / PixelRatio.get(),
+    borderRightWidth: 1 / PixelRatio.get(),
+    borderColor: '#fff',
+  },
+  lineCenter: {
+    borderBottomWidth: 1 / PixelRatio,
+    borderColor: '#fff',
+  }
+});
+
+AppRegistry.registerComponent('ViewDemo', () => ViewDemo);
